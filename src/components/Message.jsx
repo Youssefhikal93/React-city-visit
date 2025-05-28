03edff37a71c11ffefc02f64e31721c71eeb4570
youssef <youssefhikal@gmail.com>
2025-05-28 13:01:19 +0200

    modfied message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,6 @@
 function Message({ message }) {
   return (
-    <p className="text-center text-xl w-4/5 my-8 mx-auto font-semibold">
+    <p className="text-center text-xl w-4/5 my-8 mx-auto font-semibold text-light-0">
       <span role="img">👋</span> {message}
     </p>
   );
